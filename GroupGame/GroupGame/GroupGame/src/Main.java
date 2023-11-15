0bad86c2b6d662e7a898c164c5bbf0a95ef0c5f2
Ahmed Said Yazici <ahmetyazici2004@gmail.com>
2023-11-15 14:45:36 +0000

    ass

--- GroupGame/GroupGame/GroupGame/src/Main.java
+++ GroupGame/GroupGame/GroupGame/src/Main.java
@@ -4,16 +4,31 @@ public class Main {
 
 // lmao kjfdgengw
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, please select an option!\n");
+        System.out.println("1 - Make a character\n");
+        System.out.println("2 - Start game\n");
 
         Scanner scanner = new Scanner(System.in);
         int option = scanner.nextInt();
 
-        // make a character
-        switch(option) {
-            // Chose a name
-            case 1:
-                System.out.println("Choose a name for your character");
+
+
+        while(option != -1) {
+            // make a character
+            switch(option) {
+                // Chose a name
+                case 1:
+                    System.out.println("Choose a name for your character");
+                    String characterName = scanner.next();
+                    Player p = new Player(characterName);
+                    System.out.println(p.getName());
+                    playGame();
+                    break;
+            }
         }
+
+    }
+
+    private static void playGame() {
     }
 }
--- GroupGame/GroupGame/GroupGame/src/Player.java
+++ GroupGame/GroupGame/GroupGame/src/Player.java
@@ -1,2 +1,20 @@
 public class Player {
+    private int HP;
+    private String name;
+    private int inventorySize = 5;
+    private int[] items = new int[inventorySize];
+    private int strength;
+    private int intelligence;
+    private int dexterity;
+    private  int endurance;
+    private int agility;
+    private int luck;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
